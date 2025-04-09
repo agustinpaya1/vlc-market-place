b74ff9fac735ef08df972ee6f39051d9cdd52b9b
Agustín <payaagustin@gmail.com>
2025-04-09 10:56:49 +0200

    primer commit!

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vlc.market.place',
+  appName: 'vlc-market-place',
+  webDir: 'www'
+};
+
+export default config;
