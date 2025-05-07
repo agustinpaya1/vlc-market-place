cd822d8b26c455c00eaa9b7d11ef9adfe8fae07f
Agustín <payaagustin@gmail.com>
2025-05-07 17:50:46 +0200

    Cambios en los visuales de la pestaña tiendas. He cambiado la parte de las categorías y modificado la barra de navegación (soy consciente de que no se ven los logitpos)

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -8,7 +8,7 @@ import {
   IonBadge
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { storefront, map, cart, person } from 'ionicons/icons';
+import { storefront, map, cart, person, storefrontOutline, mapOutline, cartOutline, personOutline } from 'ionicons/icons';
 import { CartService } from '../services/cart.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
@@ -34,7 +34,7 @@ export class TabsPage {
     public cartService: CartService,
     private router: Router
   ) {
-    addIcons({ storefront, map, cart, person });
+    addIcons({storefrontOutline,mapOutline,cartOutline,personOutline,storefront,map,cart,person});
   }
 
   navigateToProfile() {
