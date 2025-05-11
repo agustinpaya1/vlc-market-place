3cc4b52aa82bfadf34641ac5e2aa29912370595b
Agustín <payaagustin@gmail.com>
2025-05-11 16:33:59 +0200

    cambios para que se pueda ejecutar en el movil

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'vlc.market.place',
   appName: 'vlc-market-place',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    hostname: '192.168.68.135',
+    androidScheme: 'https',
+    iosScheme: 'https',
+    url: 'http://192.168.68.135:4200'
+  }
 };
 
 export default config;
